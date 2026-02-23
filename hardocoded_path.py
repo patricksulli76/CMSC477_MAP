f57f8422bc9b165cdd4e1b9f836dc49f81262b32
mikostrzewa <mikolaj.kostrzewa1618@gmail.com>
2026-02-23 16:21:45 -0500

    Map fixed

---
+++ hardocoded_path.py
@@ -0,0 +1,96 @@
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+fig, ax = plt.subplots()
+
+# AprilTag ID -> (x, y) of the obstacle grid cell’s lower-left corner
+# (Multiple tags can share the same (x,y) because they’re mounted on the same obstacle square.)
+
+apriltag_to_grid = {
+    30: (3, 7),
+    31: (3, 7),
+    32: (3, 6),
+    33: (3, 6),
+    34: (3, 5),
+    35: (5, 8),
+    36: (7, 8),
+    37: (6, 5),
+    38: (6, 4),
+    39: (6, 4),
+    40: (6, 2),
+    41: (6, 2),
+    42: (9, 7),
+    43: (9, 7),
+    44: (9, 6),
+    45: (9, 6),
+    46: (9, 5),
+}
+
+graph = {}
+size_x = 13
+size_y = 10
+for x in range(0, size_x):
+    for y in range(0, size_y):
+        graph[(x, y)] = []
+        if x > 0:
+            graph[(x, y)].append((x - 1, y))
+        if x < size_x - 1:
+            graph[(x, y)].append((x + 1, y))
+        if y > 0:
+            graph[(x, y)].append((x, y - 1))
+        if y < size_y - 1:
+            graph[(x, y)].append((x, y + 1))
+        if x > 0 and y > 0:
+            graph[(x, y)].append((x - 1, y - 1))
+        if x < size_x - 1 and y > 0:
+            graph[(x, y)].append((x + 1, y - 1))
+        if x > 0 and y < size_y - 1:
+            graph[(x, y)].append((x - 1, y + 1))
+        if x < size_x - 1 and y < size_y - 1:
+            graph[(x, y)].append((x + 1, y + 1))
+
+def add_obstacle(x, y, graph):
+    for i in graph[(x, y)]:
+        graph[i].remove((x, y))
+    graph.pop((x, y))
+    return graph
+
+def add_rect(x, y, w=1, h=1, color='gray'):
+    ax.add_patch(Rectangle((x, y), w, h, facecolor=color))
+
+def add_point(x, y):
+    ax.scatter(x + 0.5, y + 0.5)
+
+def add_edge(x1, y1, x2, y2):
+    ax.plot([x1 + 0.5, x2 + 0.5],
+            [y1 + 0.5, y2 + 0.5], color='blue')
+
+for x in range(3, 10):
+    add_rect(x, 8)
+    add_obstacle(x, 8, graph)
+
+for y in range(4, 8):
+    add_rect(3, y)
+    add_obstacle(3, y, graph)
+for y in range(4, 8):
+    add_rect(9, y)
+    add_obstacle(9, y, graph)
+for y in range(1, 6):
+    add_rect(6, y)
+    add_obstacle(6, y, graph)
+
+add_rect(1, 5, color='limegreen')
+add_rect(11, 5, color='orangered')
+
+ax.set_xlim(0, 13)
+ax.set_ylim(0, 10)
+ax.set_aspect('equal')
+ax.set_xticks(range(0, 14))
+ax.set_yticks(range(0, 11))
+ax.grid(True)
+
+
+
+plt.title("Map")
+plt.show()
--- map_test.py
+++ map_test.py
@@ -1,19 +1,19 @@
 import map
 
-map_graph = map.Map(13,10)
+map_graph = map.Map(13,11)
 
-for x in range(3, 10):
-    map_graph.add_rect(x, 8)
-    map_graph.add_obstacle(x, 8)
-for y in range(5, 8):
+for x in range(4, 9):
+    map_graph.add_rect(x, 9)
+    #map_graph.add_obstacle(x, 8)
+for y in range(5, 10):
     map_graph.add_rect(3, y)
-    map_graph.add_obstacle(3, y)
-for y in range(5, 8):
+    #map_graph.add_obstacle(3, y)
+for y in range(5, 10):
     map_graph.add_rect(9, y)
-    map_graph.add_obstacle(9, y)
+    #map_graph.add_obstacle(9, y)
 for y in range(1, 6):
     map_graph.add_rect(6, y)
-    map_graph.add_obstacle(6, y)
+    #map_graph.add_obstacle(6, y)
 
 map_graph.add_rect(1, 5, color='limegreen')
 map_graph.add_rect(11, 5, color='orangered')
